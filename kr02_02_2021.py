97f9ad3e346f0a1ae902413fd0ab37a775a31c24
MakarovskyEgor <78807914+MakarovskyEgor@users.noreply.github.com>
2021-02-09 19:03:12 +0300

    Update kr02_02_2021.py

--- kr02_02_2021.py
+++ kr02_02_2021.py
@@ -5,9 +5,8 @@ def task_1(two_dim_words):
         Заполнять список значениями не нужно.
         Финальное значение должно быть помещено в переменную sorted_words.
         """
-
-    return sorted_words
-
+    
+    pass
 
 def task_3(numbers):
     """
@@ -15,8 +14,41 @@ def task_3(numbers):
         Переменная numbers - ваша строка чисел.
         Финальное значение должно быть помещено в переменную dict_min.
         """
+  
+    print(numbers)
+    numbers.isdigit()
+    i = 0
+    for i in range(len(numbers)):
+        numbers.split()
+        w = numbers.count('1')
+        q = numbers.count('2')
+        e = numbers.count('3')
+        r = numbers.count('4')
+        t = numbers.count('5')
+        y = numbers.count('6')
+        u = numbers.count('7')
+        o = numbers.count('8')
+        p = numbers.count('9')
+        i += 1
+    nums = dict([(1, w),
+                 (2,q),
+                 (3,e),
+                 (4,r),
+                 (5,t),
+                 (6,y),
+                 (7,u),
+                 (8,o),
+                 (9,p)])
+    for m in nums.values():
+        i+1
+    v=max(nums.values())
+    list(nums.values())
+    nums.values().pop(v)
+    l=max(nums.values())
+    nums.values().pop(l)
+    a=max(nums.values())
 
-    return dict_min
+    pass
 
 
 def task_4_1(words):
@@ -55,8 +87,13 @@ def task_5(lst1, lst2):
         Переменные lst1 и lst2 - два данных списка.
         Финальное значение должно быть помещено в переменную diff.
         """
-
-    return diff
+    
+    s_f_l=set(lst1)
+    s_f_l_1=set(lst2)
+    con = s_f_l.symmetric_difference(s_f_l_1)
+    a=list(con)
+    a.sort(reverse=False)
+    return a
 
 
 def task_6(lst):
@@ -66,5 +103,10 @@ def task_6(lst):
         Финальное значение должно быть помещено в переменную res.
         """
 
-    return res
+    m=set(lst)
+    n=list(m)
+    n.sort(reverse=True)
+    n=tuple(n)
+
+    return n
 
